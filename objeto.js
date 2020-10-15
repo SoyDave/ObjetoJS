57af06c6111499b2a40d0bfaa06d79add8167d6c
Juan D Ortiz C <soydavex@gmail.com>
2020-10-14 19:28:52 -0500

    Esto es un Objeto

---
+++ objeto.js
@@ -0,0 +1,13 @@
+let persona={
+
+    nombre:"Juan",
+    apellido:"Ortiz",
+    edad:26,
+    telefono:'3005783309',
+    ciudad:"medellin"
+
+}
+
+console.log(persona.apellido);
+
+console.log(`${persona.nombre} y el apellido es ${persona.apellido}`);
